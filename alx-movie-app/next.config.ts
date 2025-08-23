e25d201d6a779053cc32593f2b7bab943854819e
Orion <Orion45@dd.gmail.com>
2025-08-23 14:50:36 +0200

    Transforming the movie application into a Progressive Web App (PWA) using Next.js and next-pwa package

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,6 +1,7 @@
+import type { NextConfig } from 'next';
 import withPWAInit from "@ducanh2912/next-pwa";
 
-/** @type {import('next').NextConfig} */
+ // @type {import('next').NextConfig}
 
 const withPWA = withPWAInit({
   dest: 'public'
